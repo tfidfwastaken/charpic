dc46971310f00562ae3bf8bfae74d39cd7c016b2
Atharva Raykar <raykar.ath@gmail.com>
2019-06-05 23:35:25 +0530

    Add test for checking mapping

--- charpic.rkt
+++ charpic.rkt
@@ -1,7 +1,7 @@
 #lang racket/base
 (require 2htdp/image)
 
-(define (get-brightness-list name path)
+(define (get-luminosity-list name path)
   (define img-color-list
     (image->color-list (bitmap/file path)))
   (define luminosity-list
@@ -12,11 +12,30 @@
       (color-blue color-value-list)
       (color-green color-value-list)))
     luminosity-list)
-(provide get-brightness-list)
+(provide get-luminosity-list)
+
+(define (luminosity->char lum-val)
+  (define norm-factor (/ 64 256))
+  (define fill-chars
+    "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$")
+  (string-ref fill-chars (round (* norm-factor lum-val))))
+
+(define (luminosity-list->char-list lum-list)
+  (for/list ([lum-val (in-list lum-list)])
+    (luminosity->char lum-val)))
 
 (module+ main
-  (get-brightness-list "somename" "/home/atharva/Pictures/profcrop.jpg"))
+  (get-luminosity-list "somename" "/home/atharva/Pictures/profcrop.jpg"))
 
 (module+ test
   (require rackunit)
-  (check-true (list? (get-brightness-list "somename" "/home/atharva/Pictures/profcrop.jpg"))))
+
+  (check-true (list? (get-luminosity-list
+                      "somename" "/home/atharva/Pictures/profcrop.jpg")))
+
+  (test-case "checking the luminosity to character mapping"
+    (check-equal? (luminosity->char 0) #\`)
+    (check-equal? (luminosity->char 255) #\$)
+    (check-equal? (luminosity->char 73) #\{)
+    (check-true (list? (luminosity-list->char-list (list 73 255 2 124))))))
+   
